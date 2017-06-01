57a5fd5cd30696a876627c21e274e1ec627e6a5c
Allan Roque Barbosa da Silva <allanrockstifo@hotmail.com>
2017-06-01 17:50:30 -0300

    Geração primitiva do método main...

--- src/PadariaDoXico.java
+++ src/PadariaDoXico.java
@@ -1,4 +1,64 @@
+import java.util.Scanner;
 
 public class PadariaDoXico {
 
+	private static Scanner scanner;
+
+	static String menu(){
+		
+		scanner = new Scanner(System.in);
+		
+		System.out.println("                                      PADARIA DO XICO\n\n");
+		System.out.println("                                1 - INICIAR UMA VENDA");
+		System.out.println("                                2 - CADASTROS");
+		System.out.println("                                3 - SAIR\n");
+		System.out.print("                                > ");
+		
+		return scanner.nextLine();
+		
+	}
+	
+	public static void main(String[] args) {
+	
+		boolean exit = false;
+		String option;
+		
+		while(exit != true){
+			
+			option = menu();
+			
+			switch (option) {
+			
+			case "1":
+				
+				//INICIAR A VENDA
+				
+				break;
+				
+			case "2":
+				
+				// cadastro
+				
+				break;
+				
+			
+				
+			case "3":
+				System.out.println("Obrigado por comprar \"pão\" na padaria do xico!");
+				exit = true;
+				break;
+
+			default:
+				
+				System.out.println("Opção incorreta!");
+				System.out.println("Please, digite uma opção válida!");
+				break;
+				
+			}
+			
+			
+		}
+		
+	}
+	
 }
