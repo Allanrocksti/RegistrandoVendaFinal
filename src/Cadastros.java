dc3fb50380154eb8b91b4bc839c3b3c7ef3cffc3
Aluizio Neto <aluizioneto03@hotmail.com>
2017-06-04 01:48:48 -0300

    Primeiro Commit

---
+++ src/Cadastros.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+/**
+ *@date 3 de jun de 2017
+ *@author  Neto
+ *@time	21:41:50
+ */
+
+/**
+ * @author Neto
+ *
+ */
+public class Cadastros {
+	
+	private static Scanner scanner = new Scanner(System.in);
+	
+	Arquivo arquivo = new Arquivo();
+	
+	
+	Extra extra = new Extra();
+	public String produto(){
+		Produto produto;
+		String msg = "";
+		System.out.println("Nome do produto: ");
+		String nome = scanner.nextLine();
+		System.out.println("Descrição do produto: ");
+		String descricao = scanner.nextLine();
+		System.out.println("Valor do produto: ");
+		String dado = scanner.nextLine();
+		float valor = extra.verificarF(dado);
+		System.out.println("Codigo do produto: ");
+		String codigo = scanner.nextLine();
+		System.out.println("Quantidade do produto: ");
+		String qtd_produto = scanner.nextLine();
+		
+		produto = new Produto(nome,descricao,valor,codigo,qtd_produto);
+		
+		msg = Arquivo.salvarObjeto(produto);
+
+		return msg;
+	}
+}
