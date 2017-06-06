996140020090e44a63180000a529dc787db1be8d
Aluizio Neto <aluizioneto03@hotmail.com>
2017-06-06 08:04:45 -0300

    20:15

--- src/Compra.java
+++ src/Compra.java
@@ -4,8 +4,8 @@ public class Compra {
 	
 	//Pessoa Vendendor = new Pessoa();
 	//Pessoa Usuario = new Pessoa();
-	HashMap<String, Float> Carrinho = new HashMap<String, Float>();
-	Float ValorLiguido;
-	Float Desconto;
+	//HashMap<String, Float> Carrinho = new HashMap<String, Float>();
+	//Float ValorLiguido;
+	//Float Desconto;
 
 }
